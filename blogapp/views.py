0e239de6fb5f9277b7ed8257eb17b4d08cdc172d
songmody <tnejd4766@naver.com>
2019-08-07 01:01:49 +0900

    lk

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blogapp
+from drink.models import BlogDrink
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
 def home(request):
-    return render(request,'home.html')
+    blogapps = Blogapp.objects # 쿼리셋( 블로그 객체 목록 = 메소드 ) 전달 
+    return render(request, 'home.html', {'blogs' : blogs})
+
+    # 쿼리셋 표현
+    # 모델.쿼리셋(object)
+def detail(request, blogapp_id):
+    blogapp_detail = get_object_or_404(Blogapp, pk=blogapp_id)
+    return render(request, 'detail.html', {'blogapp' : blogapp_detail})
+
+def new(request):
+    return render(request, 'new.html')
+@csrf_exempt
+def create(request): # 입력받은내용을 DB에 넣어주는 함수
+    blogapp = Blogapp()
+    blogapp.title = request.POST['title']
+    blogapp.body = request.POST['body']
+    blogapp.pub_date = timezone.datetime.now()
+    blogapp.kinds = request.POST['kinds']
+    blogapp.kakaotalkID = request.POST['kakaoTalkID']
+    blogapp.price = request.POST['price']
+    blogapp.save()
+    return render(request, 'finish.html')
+    
--- teamprj/urls.py
+++ teamprj/urls.py
@@ -2,7 +2,19 @@
 from django.contrib import admin
 from django.urls import path
 import blogapp.views
+import drink.views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',blogapp.views.home),
-]
+    path('', blogapp.views.home, name = 'home'),
+    path('blog/<int:blog_id>', blogapp.views.detail, name = 'detail'),
+    path('blogapp/new/', blog.views.new, name = 'new'),
+    path('blogapp/create/', blogapp.views.create, name = 'create'),
+    path('drink/', drink.views.drink, name = 'drink'),
+    path('food/', drink.views.food, name = 'food'),
+    path('fashion/', drink.views.fashion, name = 'fashion'),
+    path('specialPrice/', drink.views.specialPrice, name = 'specialPrice'),
+    path('detail/<int:blogDrink_id>', drink.views.Drinkdetail, name = 'Drinkdetail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
