3f8493fc00fea42c491893f30b08ca17b12366d3
songmody <tnejd4766@naver.com>
2019-08-07 11:42:10 +0900

    final

--- teamprj/settings.py
+++ teamprj/settings.py
@@ -110,6 +110,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blogapp','static') 
+]
+#static파일들이 현재 어디에 있는지를 나타내는 코드 블로그앱->스태틱폴더에 존재
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#static파일들이 어디로 모일것인지 정해주는 코드
+
+
+MEDIA_URL='/media/'
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
